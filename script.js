01847cc4825a64e03da7b6481db441fd75ddb29d
AleIgleRam <85503641+AleIgleRam@users.noreply.github.com>
2021-07-24 14:35:30 +0200

    updated js file

---
+++ script.js
@@ -0,0 +1,79 @@
+//Feature #1
+let today = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[today.getDay()];
+
+let hour = today.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+
+let minutes = today.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentDate = document.querySelector("#current-date");
+currentDate.innerHTML = `${day} ${hour} : ${minutes}`;
+
+//Current Weather All Cities
+
+function searchWeather(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  let apiKey = "39875ec258d45cbbcd00f2745b4d9588";
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&APPID=${apiKey}&&units=metric`;
+
+  function displayWeather(response) {
+    console.log(response.data);
+    let currentTemperature = Math.round(response.data.main.temp);
+    let city = response.data.name;
+    let country = response.data.sys.country;
+    let weatherText = response.data.weather[0].main;
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = `${city}, ${country} ${currentTemperature} C° ${weatherText}`;
+  }
+
+  axios.get(weatherUrl).then(displayWeather);
+}
+
+let cityWeather = document.querySelector("#city-search");
+cityWeather.addEventListener("submit", searchWeather);
+
+//Current Weather Current City
+
+function showWeather(event) {
+  event.preventDefault();
+  function getCoordinates(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let units = "metric";
+    let apiKey = `39875ec258d45cbbcd00f2745b4d9588`;
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+
+    function displayWeather(response) {
+      let currentTemperature = Math.round(response.data.main.temp);
+      let city = response.data.name;
+      let country = response.data.sys.country;
+      let weatherText = response.data.weather[0].main;
+      let h1 = document.querySelector("h1");
+      h1.innerHTML = `${city}, ${country} ${currentTemperature} C° ${weatherText}`;
+    }
+
+    axios.get(weatherUrl).then(displayWeather);
+  }
+
+  navigator.geolocation.getCurrentPosition(getCoordinates);
+}
+
+let currentWeather = document.querySelector("#current-city");
+currentWeather.addEventListener("click", showWeather);
