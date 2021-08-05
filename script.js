32502cee05b8198cf4e1856528f284efe8eb80ea
AleIgleRam <85503641+AleIgleRam@users.noreply.github.com>
2021-08-05 15:04:18 +0200

    js forecast function updated

--- script.js
+++ script.js
@@ -27,33 +27,59 @@ currentDate.innerHTML = `${day} ${hour} : ${minutes}`;
 
 //Display Forecast
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="https://openweathermap.org/img/wn/50d@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max"> 18° </span>
-                  <span class="weather-forecast-temperature-min"> 12° </span>
+                  <span class="weather-forecast-temperature-max"> ${Math.round(
+                    forecastDay.temp.max
+                  )}° </span>
+                  <span class="weather-forecast-temperature-min">  ${Math.round(
+                    forecastDay.temp.min
+                  )}° </span>
                 </div>
                 <div class="humidity"> <span class="humidity-icon"><i class="fas fa-tint"></i></span> <span id="hum1">19%</span></div>
                 </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "39875ec258d45cbbcd00f2745b4d9588";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Current Weather All Cities
@@ -80,9 +106,10 @@ function searchWeather(event) {
 
     currentTemperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
-    countryElement.innerHTML = `, ${response.data.sys.country}`;
     weatherTextElement.innerHTML = response.data.weather[0].description;
     currenthumidityElement.innerHTML = `${response.data.main.humidity} %`;
+
+    getForecast(response.data.coord);
   }
 
   axios.get(weatherUrl).then(displayWeather);
@@ -118,9 +145,9 @@ function showWeather(event) {
 
       currentTemperatureElement.innerHTML = Math.round(response.data.main.temp);
       cityElement.innerHTML = response.data.name;
-      countryElement.innerHTML = `, ${response.data.sys.country}`;
       weatherTextElement.innerHTML = response.data.weather[0].description;
       currenthumidityElement.innerHTML = `${response.data.main.humidity} %`;
+      getForecast(response.data.coord);
     }
 
     axios.get(weatherUrl).then(displayWeather);
@@ -131,5 +158,3 @@ function showWeather(event) {
 
 let currentWeather = document.querySelector("#current-city");
 currentWeather.addEventListener("click", showWeather);
-
-displayForecast();
